4437e4d9940e85a8a538aca5f258358e7f8010f0
lacastrilp <lacastrilp@eafit.edu.co>
2024-05-10 21:43:47 -0500

    Ya hice la tarea de imprimir mi nombrecito

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void mi_nombre_Luis(){
+        System.out.println("Luis Alejandro Castrillon Pulgarin");
+    }
+
+    public static void main(String [] args){
+        mi_nombre_Luis();
+    }
+}
